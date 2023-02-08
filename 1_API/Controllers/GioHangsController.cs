12b10c751aa5c3964a9f8b798b635412e8c343e9
nguyenhuukhoa5462 <nguyenhuukhoa5462@gmail.com>
2023-02-08 14:59:49 +0700

    api_giohang

---
+++ 1_API/Controllers/GioHangsController.cs
@@ -0,0 +1,113 @@
+﻿using _1_API.ViewModel.ChucVu;
+using _1_API.ViewModel.GioHang;
+using Data.IRepositories;
+using Data.ModelsClass;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _1_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GioHangsController : ControllerBase
+    {
+        private IAllRepositories<GioHang> _repo;
+
+
+        public GioHangsController(IAllRepositories<GioHang> repo)
+        {
+            _repo = repo;
+
+        }
+
+        [HttpGet]
+        [Route("Get-All")]
+        public async Task<IActionResult> GetAllChucVu()
+        {
+            var result = await _repo.GetAllAsync();
+            if (result == null) return Ok("Không có giỏ hàng");
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("GetById/{id}")]
+        public async Task<IActionResult> GetChucVuById(Guid id)
+        {
+            var result = await _repo.GetByIdAsync(id);
+            if (result == null) return Ok("Không tìm thấy giỏ hàng");
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        public async Task<IActionResult> CreateChucVu([FromForm] CreateGioHang ccv)
+        {
+            GioHang cv = new GioHang()
+            {
+                Id = Guid.NewGuid(),
+                IdKH = ccv.IdKhacHang
+            };
+            try
+            {
+                var result = await _repo.AddOneAsyn(cv);
+                return Ok(cv);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Tạo mới không thành công");
+            }
+
+        }
+
+        [HttpPost]
+        [Route("Update/id")]
+        public async Task<IActionResult> UpdateChucVu(Guid id, [FromForm] CreateGioHang ucv)
+        {
+            var result = await _repo.GetByIdAsync(id);
+            if (result == null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Không tìm thấy giỏ hàng");
+            }
+            else
+            {
+                result.IdKH = ucv.IdKhacHang;
+                try
+                {
+                    await _repo.UpdateOneAsyn(result);
+                    return Ok(result);
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, "Update không thành công");
+                }
+
+
+            }
+
+        }
+        [HttpGet]
+        [Route("Delete/{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var result = await _repo.GetByIdAsync(id);
+            if (result == null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Không tìm thấy giỏ hàng");
+            }
+            else
+            {
+                try
+                {
+                    await _repo.DeleteOneAsyn(result);
+                    return Ok("Xóa thành công");
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, "Xóa không thành công");
+                }
+
+
+            }
+        }
+    }
+}
