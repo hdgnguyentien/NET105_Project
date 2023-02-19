7938705542a30a50a25e292791d6baee310cbd98
thang.nguyen <thang.nguyen@crossian.com>
2023-02-19 16:27:36 +0700

    fix

---
+++ CustomerViews/Controllers/CartController.cs
@@ -0,0 +1,29 @@
+﻿using CustomerViews.IServices;
+using Data.ModelsClass;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerViews.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ILogger<CartController> _logger;
+        private readonly IAllServices _services;
+        public CartController(ILogger<CartController> logger, IAllServices services)
+        {
+            _logger = logger;
+            _services = services;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> ThemGioHang()
+        {
+            return View();
+        }
+        public async Task<IActionResult> MuaHang()
+        {
+            return View();
+        }
+    }
+}
