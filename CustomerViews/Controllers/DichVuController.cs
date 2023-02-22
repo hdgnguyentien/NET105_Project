610aaa46a54fe16d0c230d47038902c09632669d
manhhdung <117856286+manhhdung@users.noreply.github.com>
2023-02-22 21:30:21 +0700

    dung

---
+++ CustomerViews/Controllers/DichVuController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerViews.Controllers
+{
+    public class DichVuController : Controller
+{
+    public IActionResult LienHe()
+    {
+        return View();
+    }
+        public IActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
