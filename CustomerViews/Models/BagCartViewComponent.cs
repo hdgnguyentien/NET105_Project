d0e9cdc017450f8b9616cd00455893d7d06f49d2
Nguyễn Công Tiến <hdgnguyentien@gmail.com>
2023-02-23 03:33:55 +0700

    update so luong gio hang

---
+++ CustomerViews/Models/BagCartViewComponent.cs
@@ -0,0 +1,38 @@
+﻿using CustomerViews.IServices;
+using CustomerViews.Models;
+using Data.ModelsClass;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomerViews.Helpers
+{
+    public class BagCartViewComponent : ViewComponent
+    {
+        private readonly IAllServices _services;
+        public BagCartViewComponent(IAllServices services)
+        {
+            _services = services;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var lstGHCT = await  _services.GetAll<GiohangChitiet>(Connection.api + "GioHangChiTiets/Get-All");
+            BagCartViewModel bagCartView;
+            if (lstGHCT==null||lstGHCT.Count==0)
+            {                
+                lstGHCT = null;
+                return View(lstGHCT);
+            }
+            else
+            {
+                bagCartView = new BagCartViewModel()
+                {
+                    numberOfItems = lstGHCT.Sum(x => x.SoLuong)
+                };
+                return View(bagCartView);
+            }
+            
+        }
+    }
+}
