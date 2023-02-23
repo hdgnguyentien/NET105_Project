573048a2d747a791bfdf396754cb4a0ec5c423e5
daoqpqp <109519223+daoqpqp@users.noreply.github.com>
2023-02-23 17:38:29 +0700

    update dangnhap

--- CustomerViews/Models/LoginRequest.cs
+++ CustomerViews/Models/LoginRequest.cs
@@ -7,15 +7,6 @@ namespace CustomerViews.Models
 {
     public class LoginRequest : ValidationAttribute
     {
-        private readonly IAllServices _services;
-        public LoginRequest()
-        {
-        }
-        public LoginRequest(IAllServices services)
-        {
-            _services = services;
-        }
-
         [Required(ErrorMessage = "Vui lòng không để trống")]
         public string? Email { get; set; }
 
@@ -23,16 +14,6 @@ namespace CustomerViews.Models
         [StringLength(30, MinimumLength = 6, ErrorMessage = "Mật khẩu ít nhất 6 ký tự")]
         //[CustomValidation(typeof(LoginRequest), "IsUserExists")]
         public string? MatKhau { get; set; }
-        //public async Task<ValidationResult> IsUserExists(string? MatKhau)
-        //{
-        //    var lstKH = await _services.GetAll<KhachHang>(Connection.api + "KhachHangs/Get-All");
-        //    var kh = lstKH.FirstOrDefault(p => p.Email == Email && p.MatKhau == MatKhau);
-        //    if (kh != null)
-        //    {
-        //        return ValidationResult.Success;
-        //    }
-        //    return new ValidationResult("Sai tài khoản hoặc mật khẩu");
 
-        //}
     }
 }
