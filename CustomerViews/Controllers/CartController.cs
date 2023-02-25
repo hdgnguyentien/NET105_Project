081a95560d2b708547368615f41b5cc819768e4c
Nguyễn Công Tiến <hdgnguyentien@gmail.com>
2023-02-25 13:03:49 +0700

    fix

--- CustomerViews/Controllers/CartController.cs
+++ CustomerViews/Controllers/CartController.cs
@@ -91,7 +91,6 @@ namespace CustomerViews.Controllers
         public async Task<IActionResult> Add(Guid ma, string idSize, string soluong)
         {
             string idkh = HttpContext.Session.GetString("idkh");
-            //HttpContext.Session.SetString("idSize", idSize);
             if (idkh == null)
             {
                 return RedirectToAction("DangNhap", "Login");
@@ -100,7 +99,7 @@ namespace CustomerViews.Controllers
             {
                 if (idSize == null)
                 {
-                    return View("Vui lòng chọn size");
+                    return Ok("Vui lòng chọn size");
                 }
                 else if (int.Parse(soluong) <= 0)
                 {
